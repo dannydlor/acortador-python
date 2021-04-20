0d02ab0a0e414671ca39c794b4de74c0a9eb8fd3
LordDanny <56984709+dannydlor@users.noreply.github.com>
2021-04-20 11:35:55 -0500

    Successfully Acortador

--- src/config/db.py
+++ src/config/db.py
@@ -3,9 +3,9 @@ import mariadb
 
 config = {
     'host' : 'localhost',
-    'port' : 3309,
+    'port' : 3306,
     'user' : 'root',
-    'password' : 'Maleja21*',
+    'password' : '12345678',
     'database' : 'enlaces',
 }
 
